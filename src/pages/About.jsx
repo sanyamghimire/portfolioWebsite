556681b54ad9726b8f77ed85a66817f44bdd0f82
Sanyam Ghimire <sanyamghimire@gmail.com>
2024-05-20 15:11:02 +0200

    second commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,11 +23,9 @@ function About() {
         </div>
 
         <div data-aos='fade-down' data-aos-duration='800' data-aos-anchor-placement="center-bottom"  className='flex  flex-col ml-12 mt-6 text-left'>
-          <p>I am a graduate master's student from Norwegian University of Science and Technology(NTNU).</p>
-          <p>I am passionate about improving my coding skills and have skills in frontend development.</p>
-          <p> Driven by a passion for web development, I specialize in crafting interactive and visually</p>
-          <p>appealing WebApps and Websites using React. Continuously striving for skill enhancement,</p> 
-          <p> I am enthusiastic about honing my abilities in this dynamic field.</p>
+          <p>Currently involved in research on assessing in-situ rock stress for safeguarding unlined pressure tunnels of hydropower.</p>
+          <p>I am passionate about improving my engineering skills and have skills in hydropower development.</p>
+          <p> Driven by a passion for project design, I specialize in sediment management in hydropower</p>
 
           <p className='mt-4'><span className='text-blue-800'>Email:</span> <a href="mailto:sanyam.ghimire@ntnu.no">sanyam.ghimire@ntnu.no</a></p>
           <p className='mt-4'><span className='text-blue-800'>Place:</span> Åsvangvegen 22B, Trondheim, Norway</p>
